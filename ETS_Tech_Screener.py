2ac49f593266b8d5a9074595a8e6aafc436c387f
Gurpreet Singh <g.singh6343@gmail.com>
2021-07-30 13:15:33 -0400

    ch2

--- ETS_Tech_Screener.py
+++ ETS_Tech_Screener.py
@@ -1,123 +1,115 @@
+import os
 import sys
 import pandas as pd
-import json  
+import json
 import nltk
 from nltk.tokenize import word_tokenize
 
-#Function to read input file
+# Function to read input file
 def readfile(filename):
-    #Reading csv file with pandas
+    # Reading csv file with pandas
     try:
         fl = pd.read_csv(filename)
-
+    except FileNotFoundError as e:
+        raise e
     except Exception as e:
-        raise e  
+        raise e
 
-    if 'Id' not in fl.columns:
+    if "Id" not in fl.columns:
         raise Exception("Invalid columns - need Id Column in file")
-    
-    if 'Sentence' not in fl.columns:   
-        raise Exception("Invalid columns - need Sentence column in file") 
+
+    if "Sentence" not in fl.columns:
+        raise Exception("Invalid columns - need Sentence column in file")
 
     return fl
 
-#Function to create json 
+
+# Function to create json
 def feature_creator(sentence_id, sentence):
-    
-    #Word tokenization
+
+    # Word tokenization
     words = word_tokenize(sentence)
 
-    #Tagging
+    # Tagging
     pos_with_words = nltk.pos_tag(words)
 
-    #Separating tags & words and saving tags separately
+    # Separating tags & words and saving tags separately
     tags = []
     for pos in pos_with_words:
         tags.append(pos[1])
 
-    preposition = ['on','for','of','to','at','in','with','by']  
-    
+    preposition = ["on", "for", "of", "to", "at", "in", "with", "by"]
+
     length = len(words)
 
     preposition_index = []
-    for ind,word in enumerate(words):
-        #Checking specifically for 'to' 
-        if word.lower() == 'to' and ind+1 <length and tags[ind+1]=='VB':
-            continue 
+    for ind, word in enumerate(words):
+        # Checking specifically for 'to'
+        if word.lower() == "to" and ind + 1 < length and tags[ind + 1] == "VB":
+            continue
         if word.lower() in preposition:
             preposition_index.append(ind)
 
-    print(preposition_index)  
+    print(preposition_index)
     print(tags)
-    
-    
-    
+
     for i in preposition_index:
 
+        w, t = words[i], tags[i]
 
-        w,t = words[i], tags[i]
-        
         # Handling out of index error on both sides of list
-        if i-1<0:
-            wBack1,tBack1 = None,None
-            wBack2,tBack2=None, None
+        if i - 1 < 0:
+            wBack1, tBack1 = None, None
+            wBack2, tBack2 = None, None
         else:
-            wBack1,tBack1 = words[i-1], tags[i-1]
-            if i-2<0:
-                wBack2,tBack2 = None,None
-            else:      
-                wBack2,tBack2 =  words[i-2], tags[i-2]
-        if i+1>length-1:
-            wForward1, tForward1=None, None
-            wForward2,tForward2 = None, None
-        else:
-            wForward1, tForward1 = words[i+1], tags[i+1]
-            if i+2>length-1:
-                wForward2,tForward2=None, None
+            wBack1, tBack1 = words[i - 1], tags[i - 1]
+            if i - 2 < 0:
+                wBack2, tBack2 = None, None
             else:
-                wForward2,tForward2 = words[i+2], tags[i+2]
+                wBack2, tBack2 = words[i - 2], tags[i - 2]
+        if i + 1 > length - 1:
+            wForward1, tForward1 = None, None
+            wForward2, tForward2 = None, None
+        else:
+            wForward1, tForward1 = words[i + 1], tags[i + 1]
+            if i + 2 > length - 1:
+                wForward2, tForward2 = None, None
+            else:
+                wForward2, tForward2 = words[i + 2], tags[i + 2]
 
-        #Save features in list ls
+        # Save features in list ls
         ls = []
 
-        ls.append(" ".join(filter(None,[wBack1,w])))
-        ls.append(" ".join(filter(None,[w,wForward1])))
-        ls.append(" ".join(filter(None,[wBack1,w, wForward1])))
-        ls.append(" ".join(filter(None,[wBack2,wBack1,w])))
-        ls.append(" ".join(filter(None,[w,wForward1,wForward2])))
-        ls.append(" ".join(filter(None,[wBack2,wBack1,w,wForward1,wForward2])))
-        ls.append(" ".join(filter(None,[tBack1,t])))
-        ls.append(" ".join(filter(None,[t,tForward1])))
-        ls.append(" ".join(filter(None,[tBack1,t, tForward1])))
-        ls.append(" ".join(filter(None,[tBack2,tBack1,t])))
-        ls.append(" ".join(filter(None,[t,tForward1,tForward2])))
-        ls.append(" ".join(filter(None,[tBack2,tBack1,t,tForward1,tForward2])))
+        ls.append(" ".join(filter(None, [wBack1, w])))
+        ls.append(" ".join(filter(None, [w, wForward1])))
+        ls.append(" ".join(filter(None, [wBack1, w, wForward1])))
+        ls.append(" ".join(filter(None, [wBack2, wBack1, w])))
+        ls.append(" ".join(filter(None, [w, wForward1, wForward2])))
+        ls.append(" ".join(filter(None, [wBack2, wBack1, w, wForward1, wForward2])))
+        ls.append(" ".join(filter(None, [tBack1, t])))
+        ls.append(" ".join(filter(None, [t, tForward1])))
+        ls.append(" ".join(filter(None, [tBack1, t, tForward1])))
+        ls.append(" ".join(filter(None, [tBack2, tBack1, t])))
+        ls.append(" ".join(filter(None, [t, tForward1, tForward2])))
+        ls.append(" ".join(filter(None, [tBack2, tBack1, t, tForward1, tForward2])))
 
-        
-        output={}
-        output["id"] = str("_".join([str(sentence_id),str(i)]))
+        output = {}
+        output["id"] = str("_".join([str(sentence_id), str(i)]))
         output["prep"] = words[i]
         output["features"] = ls
-        
-        #Saving json file with same name as ID
-        with open("Output_Files/"+output["id"]+".json", "w") as outfile: 
-            json.dump(output, outfile)        
-           
-        #print(output) 
 
+        # Saving json file with same name as ID
+        with open("Output_Files/" + output["id"] + ".json", "w") as outfile:
+            json.dump(output, outfile)
 
+        # print(output)
 
-  
 
 if __name__ == "__main__":
-    
+
     filename = sys.argv[1]
     file = readfile(filename)
 
-    #Calling function over csv file
+    # Calling function over csv file
     for i in range(file.shape[0]):
-       feature_creator(file['Id'][i], file['Sentence'][i])
-
-	
-
-
+        feature_creator(file["Id"][i], file["Sentence"][i])
--- Testing.py
+++ Testing.py
@@ -1,15 +1,21 @@
 import unittest
-import ETS_Tech_Screener as code 
+import pandas as pd
+import ETS_Tech_Screener as code
 
 
 class TestETS(unittest.TestCase):
+    def test_InputFile(self):
+        # TestCase: File not found
+        self.assertRaises(FileNotFoundError, code.readfile, "sample_errored.csv")
+        # TestCase: Invalid columns
+        self.assertRaises(Exception, code.readfile, "sample_wrongcols.csv")
 
-	def test_InputFile(self):
-		self.assertRaises(Exception, code.readfile, 'sample_errored.csv')
-		self.assertRaises(Exception, code.readfile, 'sample_wrongcols.csv')  
+        # TestCase: Correct file input
+        assert isinstance(code.readfile("sentences.csv"), pd.DataFrame)
 
-	def test_Output(self):
-		assert True
+    def test_Output(self):
+        assert True
 
-if __name__ =="__main__":
-    unittest.main() 		
+
+if __name__ == "__main__":
+    unittest.main()
